7c5cc192e44e23792fc11179a0147c32d589a9e0
KillermenBE <davy.de.coster@gmail.com>
2015-02-23 13:34:28 +0100

    Processing code added

--- app/TestVideo.js
+++ app/TestVideo.js
@@ -5,18 +5,20 @@
     var getPixels   = require('get-pixels');
     var ndarray     = require("ndarray");
 
+var ledData = new Buffer((400*3)+3);
+
 try {
-    new ffmpeg('../upload/test.mp4', function (err, video) {
+    new ffmpeg('../upload/red.mp4', function (err, video) {
         if (!err) {
-            video.fnExtractFrameToJPG('../upload/test', {
+            video.fnExtractFrameToJPG('../upload/red', {
                 frame_rate : 25,
-                size: 175+'x'+100,
-                file_name : '%i'
+                size: 107+'x'+48,
+                file_name : 'Screen_%i'
             }, function (error, files) {
                 if (!error)
-                    console.log('Frames: ' + files);
 
                 var newFiles = files.toString().split(',');
+
                 for(var i = 1, len = newFiles.length; i<len; i++){
                     getPixels(newFiles[i], "image/jpeg", function(err, pixels){
                         if(err){
@@ -25,17 +27,57 @@ try {
                         else
                         {
                             var array = ndarray(pixels.data, pixels.shape, pixels.stride, pixels.offset);
-                            var ledData = [];
+                            var pixel = [];
+                            var offset = 3;
+                            var linesPerPin = 48/8;
+                            var xbegin;
+                            var xend;
+                            var xinc;
 
-                            for(var i = 0, len = pixels.shape[0]; i< len; i++)
+                            for (var y = 0; y <linesPerPin; y++)
                             {
-                                for(var ii = 0, lenn = pixels.shape[1]; ii<lenn; ii++)
-                                {
-                                    ledData.push([array.get(ii,i,0),array.get(ii,i,1),array.get(ii,i,2)]);
+                                if((y&1)==(0)){
+                                    xbegin = 0;
+                                    xend = 107;
+                                    xinc = 1;
                                 }
-                            }
+                                else
+                                {
+                                    xbegin = 106;
+                                    xend = -1;
+                                    xinc = -1;
+                                }
+                                for(var x = xbegin; x!=xend; x+=xinc)
+                                {
+                                    for(var ii = 0; ii<8; ii++)
+                                    {
+                                        //Pixels from left to right, rows to down, RGB
+                                        pixel[ii] = (array.get(ii,x,1)<<16)|(array.get(ii,x,0)<<8)|array.get(ii,x,2);
+                                    }
 
-                            console.log('px');
+                                    for(var mask = 0x800000; mask != 0; mask >>= 1)
+                                    {
+                                        var b = 0;
+                                        for (var iii=0; iii<8;iii++)
+                                        {
+                                            if((pixel[iii] & mask) !=0) {
+                                                b |= (1 << iii);
+                                                console.log(b);
+                                            }
+                                        }
+                                        ledData[offset++] = b;
+                                    }
+                                }
+
+                                ledData[0] = '*';
+                                var usec =((1000000.0 / 25) * 0.75);
+                                var byte = usec & 0xff;
+
+                                ledData[1] = byte;
+                                ledData[2] = (byte >>8);
+
+                                console.log(ledData.readUInt32BE(3));
+                            }
                         }
                     });
                 }
@@ -48,3 +90,11 @@ try {
     console.log(e.code);
     console.log(e.msg);
 }
+
+var sortstring = function (a, b)    {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    if (a < b) return 1;
+    if (a > b) return -1;
+    return 0;
+};
